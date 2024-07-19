7e1e2a3faee5ff230098a39cb7cee6d08fef413a
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-07-19 11:24:08 +0200

    Inital commit with all CRUD Opereations

---
+++ application/src/main/java/com/tutorials/application/repositories/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.tutorials.application.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tutorials.application.models.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
